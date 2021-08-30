ca05d5b93cd3b2e0e613dde782899969514daf50
junkiYoon <juneki4933@gmail.com>
2021-08-30 20:10:53 +0900

    CREATE student sqlalchemy model

---
+++ server/core/models/student.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, VARCHAR
+
+from server.core.models import Base
+
+
+class Student(Base):
+    __tablename__ = "student"
+
+    number = Column(VARCHAR(4), primary_key=True)
+    name = Column(VARCHAR(4), nullable=False)
