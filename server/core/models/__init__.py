2db0d6998df54c1b9011b1310a09b858a67d295e
junkiYoon <juneki4933@gmail.com>
2021-08-30 20:10:20 +0900

    CREATE sqlalchemy init
    create session_scope, and Base

---
+++ server/core/models/__init__.py
@@ -0,0 +1,28 @@
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+
+@contextmanager
+def session_scope():
+    engine = create_engine(
+        "mysql+mysqldb://root:wnsrl9098@localhost:3306/db_pej_test?charset=utf8",
+        encoding="utf-8",
+        pool_recycle=3600,
+        pool_size=20,
+        max_overflow=20,
+        pool_pre_ping=True
+    )
+    Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+Base = declarative_base()
