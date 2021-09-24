e8390c9de72fc577a6f8267ee436163c13780ef3
junkiYoon <juneki4933@gmail.com>
2021-09-24 18:54:42 +0900

    ADD checker router
    app includes checker router

--- server/main.py
+++ server/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 
 import uvicorn
 
+from server.apps import checker
+
 
 app = FastAPI()
 
+app.include_router(checker.router)
 
 if __name__ == '__main__':
     uvicorn.run("server.main:app", reload=True)
