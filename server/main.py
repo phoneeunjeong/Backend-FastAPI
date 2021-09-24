82869c884b08d7a1c4f7268c86a27ef8d20c51e4
junkiYoon <juneki4933@gmail.com>
2021-09-24 18:56:02 +0900

    ADD teacher router
    app includes teacher router

--- server/main.py
+++ server/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 
 import uvicorn
 
-from server.apps import checker
+from server.apps import checker, teacher
 
 
 app = FastAPI()
 
 app.include_router(checker.router)
+app.include_router(teacher.router)
 
 if __name__ == '__main__':
     uvicorn.run("server.main:app", reload=True)
